019371311fe7ce0e66887b6501ddf593d641a582
Jodi Alcantara <jjalcantara1@student.hau.edu.ph>
2024-01-19 22:22:49 +0800

    login fix

---
+++ src/screens/LoginPage.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'; // Import React and useState
+import {
+  Container,
+  SignUpContainer,
+  SignInContainer,
+  Form,
+  Title,
+  Input,
+  Button,
+  GhostButton,
+  OverlayContainer,
+  Overlay,
+  LeftOverlayPanel,
+  RightOverlayPanel,
+} from '../components/loginComponents'; // Import named exports individually
+
+
+const LoginPage=() =>{
+  const [signIn, toggle] = useState(true); // Use useState instead of React.useState
+  return (
+    <Container className="app-container">
+      <div className="image-overlay"></div>
+      <SignInContainer signinIn={signIn}>
+        <Form>
+          <Title>Log in</Title>
+          <Input type='email' placeholder='Username' />
+          <Input type='password' placeholder='Password' />
+          <Button>Login</Button>
+        </Form>
+      </SignInContainer>
+      <OverlayContainer signinIn={signIn}>
+        <Overlay signinIn={signIn}>
+          <LeftOverlayPanel signinIn={signIn}>
+            <div> <i className="fa-solid fa-hotel"></i> </div>
+            <Title>GoGet</Title>
+            <Title>(a room)</Title>
+            <GhostButton onClick={() => toggle(true)}>Sign In</GhostButton>
+          </LeftOverlayPanel>
+          <RightOverlayPanel signinIn={signIn}>
+            <Title>GoGet</Title>
+            <Title>(a room)</Title>
+            <GhostButton onClick={() => toggle(false)}>Sign Up</GhostButton>
+          </RightOverlayPanel>
+        </Overlay>
+      </OverlayContainer>
+    </Container>
+  );
+}
+
+export default LoginPage;
