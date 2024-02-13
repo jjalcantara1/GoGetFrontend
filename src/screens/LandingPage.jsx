189c6c11626e4df935ee56a9e0bec331cc4c78c9
christiandeness <deness.christianbenjamin@gmail.com>
2024-02-14 07:49:06 +0800

    landing page

--- src/screens/LandingPage.jsx
+++ src/screens/LandingPage.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from 'react';
+import LandingPageWallpaper from '../LandingPageWallpaper.jpg';
+import Header from '../components/Header';
+import './LandingPage.css';
+import { Link } from 'react-router-dom'
+import { button } from 'react-bootstrap'
+
+const LandingStyle = {
+  position: 'absolute', // Change to absolute
+  top: '0px',
+  left: '0px',
+  right: '0px',
+  bottom: '0px',
+  backgroundImage: `url(${LandingPageWallpaper})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+};
+
+const OverlayStyle = {
+  position: 'relative', // Change to relative
+  top: '40vh',
+  left: '0',
+  right: '0',
+  bottom: '0',
+  backgroundColor: 'rgba(0, 31, 63, 0.5)', // Semi-transparent black background
+  color: '#fff', // White text color
+  display: 'block', // Change from 'flex' to 'block'
+  height: '60px', // Add a fixed height
+  padding: '70px',
+  
+};
 
 function LandingPage() {
   return (
-    <Header>
-
-    </Header>
-  )
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      <div style={LandingStyle}>
+        <Header />
+      </div>
+      <div style={OverlayStyle}>
+        <h1 className='HotelName'>
+               GoGet      
+        </h1>
+      
+      <div className='button'>
+      <Link to="/selectroom" className="bookNow">
+        BOOK NOW
+        </Link>
+      </div>
+        
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
